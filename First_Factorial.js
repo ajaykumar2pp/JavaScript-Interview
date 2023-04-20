60d2122d14aef8d32e1778fc019cecf6b70ae317
Ajay Kumar <102378038+ajaykumar2pp@users.noreply.github.com>
2023-04-20 08:23:15 +0530

    interview factorial by coderbyte

---
+++ First_Factorial.js
@@ -0,0 +1,26 @@
+// First Factorial
+
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+
+// function FirstFactorial(num) { 
+//     return (num === 1 ? 1 : num * FirstFactorial(num - 1));
+// }
+   
+// FirstFactorial(4);
+// console.log(FirstFactorial(4))
+
+//   or 
+
+
+// function FirstFactorial(num){
+//    if(num===0 || num ===1){
+//        return 1;
+//    }
+//    else{
+//       return num * FirstFactorial(num-1);
+//    }
+
+// }
+// FirstFactorial(8)
+// console.log(FirstFactorial(8))
