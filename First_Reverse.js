e6370176dee1d224f302d248c168b4ebb2e44753
Ajay Kumar <102378038+ajaykumar2pp@users.noreply.github.com>
2023-04-20 08:55:36 +0530

    Reverse String Interview by Coderbyte

---
+++ First_Reverse.js
@@ -0,0 +1,10 @@
+// Input: This is Ajay
+// Output : yajA si sihT
+
+function FirstReverse(string){
+    return string.split("").reverse().join("")
+    // return string.split(" ").reverse().join("")
+
+}
+FirstReverse("This is Ajay")
+console.log(FirstReverse("This is Ajay")) 
